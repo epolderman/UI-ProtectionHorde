fe82ed410eb2552f1b8c80d43b25ab09e2a46682
Erik Polderman <eriksnoww@gmail.com>
2019-05-21 23:41:26 -0600

    power up debugging, add new material to super speed

--- Source/HordeMode/Private/HMPowerUpActor.cpp
+++ Source/HordeMode/Private/HMPowerUpActor.cpp
@@ -15,12 +15,18 @@ void AHMPowerUpActor::BeginPlay()
 
 void AHMPowerUpActor::OnTickPowerUp()
 {
+	UE_LOG(LogTemp, Warning, TEXT("TickedProcessed before %i"), TickedProcessed);
+
 	TickedProcessed++;
 
 	OnPowerUpTicked();
 
+	UE_LOG(LogTemp, Warning, TEXT("TickedProcessed after %i"), TickedProcessed);
+	
+
 	if (TickedProcessed >= TotalNumberOfTicks) {
 		OnExpired();
+		UE_LOG(LogTemp, Warning, TEXT("OnExpired()"));
 
 		// remove timer
 		GetWorldTimerManager().ClearTimer(TimerHandle_PowerUpTick);
@@ -29,13 +35,17 @@ void AHMPowerUpActor::OnTickPowerUp()
 
 void AHMPowerUpActor::ActivatePowerUp()
 {
+	// blueprint implemented
 	OnActivated();
 
+	UE_LOG(LogTemp, Warning, TEXT("PowerupInterval %f"), PowerUpInterval);
+	UE_LOG(LogTemp, Warning, TEXT("total number of ticks %i"), TotalNumberOfTicks);
 
 	if (PowerUpInterval > 0.0f)
 		GetWorldTimerManager().SetTimer(TimerHandle_PowerUpTick, this, &AHMPowerUpActor::OnTickPowerUp, PowerUpInterval, true);
 	else
 		OnTickPowerUp();
+
 }
 
 
