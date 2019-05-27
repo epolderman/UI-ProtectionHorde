78645111c1001859709681f1ddd9c243be4bfc03
Erik Polderman <eriksnoww@gmail.com>
2019-05-27 00:33:08 -0600

    WIP: GameMode Wave Logic with AI

--- Source/HordeMode/Private/HMGameMode.cpp
+++ Source/HordeMode/Private/HMGameMode.cpp
@@ -1,5 +1,45 @@
 #include "HordeMode/Public/HMGameMode.h"
 
+AHMGameMode::AHMGameMode()
+{
+	TimeBetweenWaves = 2.0f;
+}
 
+void AHMGameMode::StartPlay()
+{
+	Super::StartPlay();
 
+	InitNextWave();
+}
 
+void AHMGameMode::StartWave()
+{
+	WaveCount++;
+
+	NumberOfBotsToSpawnInCurrentWave = 2 * WaveCount;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &AHMGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+}
+
+void AHMGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	InitNextWave();
+}
+
+void AHMGameMode::InitNextWave()
+{
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &AHMGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void AHMGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NumberOfBotsToSpawnInCurrentWave--;
+
+	if (NumberOfBotsToSpawnInCurrentWave <= 0)
+	EndWave();
+}
--- Source/HordeMode/Public/HMGameMode.h
+++ Source/HordeMode/Public/HMGameMode.h
@@ -6,7 +6,7 @@
 
 /*
 	Generates all class files for our project, GameState, GameSession, Controller, etc..
-	Should exist only on the server when networked. GameState class handles replicated information.
+	Should exist only on the server when networked. GameState [not this] class handles replicated information.
 	Gives us MatchState, Delay, etc.. See GameModeBase.h
 */
 
@@ -14,4 +14,32 @@ UCLASS()
 class HORDEMODE_API AHMGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+public:
+	AHMGameMode();
+
+	virtual void StartPlay() override;
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category="GameMode")
+	void SpawnNewBot();
+
+	// start spawning
+	void StartWave();
+
+	// stop spawning
+	void EndWave();
+
+	// set time for next start wave
+	void InitNextWave();
+
+	void SpawnBotTimerElapsed();
+
+	// easily terminated
+	FTimerHandle TimerHandle_BotSpawner;
+
+	int32 NumberOfBotsToSpawnInCurrentWave;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category="Wave")
+	float TimeBetweenWaves;
 };
